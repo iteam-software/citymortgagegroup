2e455d1150bf782d0fe19f8d6ddd44d3af808c9c
alucero270 <a.lucero2892@gmail.com>
2023-10-30 14:27:51 -0600

    fix(google analytics): removed  gtag plug in for older plugin analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import SEO from "../components/SEO";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import { useWindowSize } from "../hooks/useWindowSize";
+import {GatsbyGoogleAnalytics} from "gatsby-plugin-google-analytics";
 export const WindowSizeContext = React.createContext();
 
 export default ({ children }) => {
@@ -13,6 +14,8 @@ export default ({ children }) => {
       <WindowSizeContext.Provider value={windowSize}>
         <Nav />
         <SEO />
+        <GatsbyGoogleAnalytics />
+
         <div className="bg-background">{children}</div>
         <Footer />
         <Global
